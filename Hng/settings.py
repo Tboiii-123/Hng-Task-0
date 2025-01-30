b45b6f698d2b71a07d63f949e97d07f76ec9dd5a
Lawal Hussein <132585505+Tboiii-123@users.noreply.github.com>
2025-01-30 18:54:02 +0100

    Update settings.py

--- Hng/settings.py
+++ Hng/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+     
+
 ]
 
 ROOT_URLCONF = 'Hng.urls'
